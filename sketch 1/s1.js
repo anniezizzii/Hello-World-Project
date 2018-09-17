50a9487e3bd2a9f5cd1a9dd6f99a9b75d2689125
anniez <43242224+anniezizzii@users.noreply.github.com>
2018-09-17 16:16:36 -0400

    backgorund color changing

--- sketch 1/s1.js
+++ sketch 1/s1.js
@@ -1,11 +1,20 @@
-function setup(){
-	createCanvas(600,600);
+var clickdistance;
 
+function setup(){
+    createCanvas(720,480);
 }
 
 function draw(){
-	background(random(100));
-	for(var i = 0, i < 10; i ++){
-		ellipse(random(width),random(height),10);
-	}
+	background(mouseX, mouseY, 0);
+		if(clickdistance < 20){
+       		 fill(mouseY,mouseX);
+   		 }else{
+     	   fill(255);
+  		  }
+    ellipse(mouseX, mouseY, 80, 80);
+}
+
+function mouseClicked(){
+	//thexy of our circle and the place that we click
+	 clickdistance = dist(mouseX, mouseY, mouseX,mouseY);
 }
