1327ea3f519b15f47b93fcf6e1345663081c8ee2
anniez <43242224+anniezizzii@users.noreply.github.com>
2018-09-17 17:40:11 -0400

    flower

--- sketch 4/s4.js
+++ sketch 4/s4.js
@@ -1,46 +1,29 @@
-function preload(){
-  sound = loadSound('assets/Damscray_DancingTiger.mp3');
-}
-
-function setup(){
-  var cnv = createCanvas(100,100);
-  cnv.mouseClicked(togglePlay);
-  fft = new p5.FFT();
-  sound.amp(0.2);
-}
-
-function draw(){
+var r = 0;
+ 
+function setup() {
+  createCanvas(600,600);
   background(0);
-
-  var spectrum = fft.analyze();
-  noStroke();
-  fill(0,255,0); // spectrum is green
-  for (var i = 0; i< spectrum.length; i++){
-    var x = map(i, 0, spectrum.length, 0, width);
-    var h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h )
-  }
-
-  var waveform = fft.waveform();
-  noFill();
-  beginShape();
-  stroke(255,0,0); // waveform is red
+}
+ 
+function draw() {  
+  //change the center
+  translate(width/2, height/2);
+  rotate(r);
+  stroke(mouseX, mouseY);
   strokeWeight(1);
-  for (var i = 0; i< waveform.length; i++){
-    var x = map(i, 0, waveform.length, 0, width);
-    var y = map( waveform[i], -1, 1, 0, height);
-    vertex(x,y);
-  }
-  endShape();
 
-  text('click to play/pause', 4, 10);
+  for(var t = 0; t< 10; t++){
+    ellipse(t*20, 0, mouseY/(t+1), mouseX/(t+1));
+  } 
+  r = r + 0.5;
 }
 
-// fade sound if mouse is over canvas
-function togglePlay() {
-  if (sound.isPlaying()) {
-    sound.pause();
-  } else {
-    sound.loop();
+function mouseClicked(){
+ if(mouseX < width/2 || mouseY <height/2){
+    fill(random(0,255), random(255,0), 0, 30);
+  } else{
+    fill(mouseX, 0, mouseY, 30);
   }
+
+  background(0);
 }
