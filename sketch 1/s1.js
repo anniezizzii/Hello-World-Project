46c20c1925d3ed9c17cb2d9140995533eb76e8f2
anniez <43242224+anniezizzii@users.noreply.github.com>
2018-09-17 16:46:21 -0400

    random rects, click the circle to change color. Add music, but it did not work.

--- sketch 1/s1.js
+++ sketch 1/s1.js
@@ -1,20 +1,33 @@
 var clickdistance;
+//var sound;
 
 function setup(){
     createCanvas(720,480);
+   // sound = loadSound('asset/sou.mp3');
 }
 
 function draw(){
 	background(mouseX, mouseY, 0);
-		if(clickdistance < 20){
-       		 fill(mouseY,mouseX);
+	circle1();
+	circles();
+}
+
+function circle1(){
+	ellipse(width/2, height/2, 100);
+}
+
+function circles(){
+	if(clickdistance < 20){
+       		 fill(mouseY, mouseX, 255);
    		 }else{
-     	   fill(255);
+     	   fill(0);
   		  }
-    ellipse(mouseX, mouseY, 80, 80);
+    for(var i = 0; i < 30; i ++){
+		rect (random(width), random(height),random(20,40),random(20,50));
+	}
 }
 
 function mouseClicked(){
-	//thexy of our circle and the place that we click
-	 clickdistance = dist(mouseX, mouseY, mouseX,mouseY);
+	 clickdistance = dist(width/2, height/2, mouseX, mouseY);
+	// sound.play();
 }
