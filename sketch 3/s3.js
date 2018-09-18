b1db59e6e1ddb9f40ddddf8d419613bc523ccab1
anniez <43242224+anniezizzii@users.noreply.github.com>
2018-09-17 20:38:12 -0400

    the ball doesn't move

--- sketch 3/s3.js
+++ sketch 3/s3.js
@@ -1,39 +1,3 @@
-// var mysound;
-
-// var bubble;
-
-// function setup(){
-// 	createCanvas(720,480);
-// 	mysound = createAudio("industry_mad.wav");
-// 	bubble = {
-// 		x: width/2,
-// 		y: height/2,
-// 		r:15
-// 	};
-// }
-
-// function draw(){
-// 	//ellipse(width/2,height/2,30);
-// 	ellipse(bubble.x, bubble.y, bubble.r * 2);
-// 	bubble.x = bubble.x + random(-5,5);
-// 	bubble.y = bubble.y + random(-5,5);
-// 	if(bubble.x >width || bubble.x <0){
-// 		bubble.x = width/2;
-// 	}
-// 	if(bubble.y >height || bubble.y <0){
-// 		bubble.y = height/2;
-// 	}
-// }
-
-// function mouseClicked(){
-// 	//thexy of our circle and the place that we click
-// 	var clickdistance = dist(width/2, height/2, mouseX,mouseY);
-// 	if(clickdistance<15){
-// 		fill(100);
-// 		mysound.play();
-// 	}
-// }
-
 var mic;
 var x = [];
 var y = [];
@@ -53,7 +17,7 @@ function draw() {
   fill(127);
  // var h = map(vol, 0, 1, height, 0);
 
-  ellipse(x, y, 30);
+  ellipse(x, y, 10);
   x = x + xspeed;
   y = y + yspeed;
  // ellipse(width/2, h - 25, 50, 50);
