e91b2b5089728857f82520a165d03305a762bbc4
anniez <43242224+anniezizzii@users.noreply.github.com>
2018-09-17 21:47:11 -0400

    rotating 3D boxes

--- sketch 2/s2.js
+++ sketch 2/s2.js
@@ -3,8 +3,35 @@ function setup() {
 }
 
 function draw() {
-  background(200);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  box(50);
+  background(0);
+
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
+  box(20);
+  fill(255,255,0,120);
+
+  rotateX(frameCount * 0.03);
+  rotateY(frameCount * 0.03);
+  box(30);
+  fill(255,0,255,30);
+
+  rotateX(frameCount * 0.04);
+  rotateY(frameCount * 0.04);
+  box(40);
+  fill(0,mouseY,mouseX,50);
+
+  rotateX(frameCount * 0.06);
+  rotateY(frameCount * 0.06);
+  box(60);
+  fill(0,mouseX,mouseY,30);
+
+  rotateX(frameCount * 0.1);
+  rotateY(frameCount * 0.1);
+  box(100);
+  fill(mouseY,mouseX,100,50);
+
+  rotateX(frameCount * 0.2);
+  rotateY(frameCount * 0.2);
+  box(200);
+  fill(mouseX,mouseY,100,20);
 }
