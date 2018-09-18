ab13b3a4f8fedf566e3a0c6082f431d7d255e5b5
anniez <43242224+anniezizzii@users.noreply.github.com>
2018-09-17 20:28:19 -0400

    a moving ball that can be control by the mic

--- sketch 3/s3.js
+++ sketch 3/s3.js
@@ -34,20 +34,38 @@
 // 	}
 // }
 
-var song;
+var mic;
+var x = [];
+var y = [];
 
 function setup() {
-  song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
-  createCanvas(720, 200);
-  background(255,0,0);
+  createCanvas(300,300);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
-function mousePressed() {
-  if ( song.isPlaying() ) { // .isPlaying() returns a boolean
-    song.stop();
-    background(255,0,0);
-  } else {
-    song.play();
-    background(0,255,0);
-  }
+function draw() {
+  translate(width/2,height/2);
+  background(0);
+
+  // Get the overall volume (between 0 and 1.0)
+  var vol = mic.getLevel();
+  fill(127);
+ // var h = map(vol, 0, 1, height, 0);
+
+  ellipse(x, y, 30);
+  x = x + xspeed;
+  y = y + yspeed;
+ // ellipse(width/2, h - 25, 50, 50);
+}
+
+function addparticle(){
+	var speed = vol + 1;
+	var clickposition = {
+
+		x: random(width),
+		y: random(height),
+		xspeed:speed,
+		yspeed:-speed,
+	}
 }
